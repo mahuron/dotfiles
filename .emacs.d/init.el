06c6df73cd491ef344cd0b5e36179312ae109a22
Ian Mahuron <ian@mahuron.org>
2018-12-27 02:50:33 -0800

    base16 theme package is not well maintained.  Use local theme.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,6 +6,10 @@
   (kill-emacs)
   )
 
+;;; User lisp
+(let ((default-directory  "~/.emacs.d/lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
 ;;; Bootstrap packaging support
 (require 'package)
 (setq package-enable-at-startup nil)
@@ -18,10 +22,8 @@
   (package-install 'use-package))
 
 (eval-when-compile
-(setq make-backup-files nil)  (require 'use-package))
-
-(use-package magit
-  :ensure t)
+  (setq make-backup-files nil)
+  (require 'use-package))
 
 (setq custom-file "~/.emacs.d/custom.el")
 (unless (file-exists-p custom-file)
@@ -32,13 +34,18 @@
 (setq inhibit-startup-screen t)
 (menu-bar-mode -1)
 
-(use-package base16-theme
-  :ensure t
-  :init
-  (setq base16-theme-256-color-source "colors")
-  :config
-  (load-theme 'base16-default-dark t)
-  )
+(defun set-background-for-terminal (&optional frame)
+  (or frame (setq frame (selected-frame)))
+  "unsets the background color in terminal mode"
+  (unless (display-graphic-p frame)
+    (set-face-background 'default nil frame)))
+(add-hook 'after-make-frame-functions 'set-background-for-terminal)
+(add-hook 'window-setup-hook 'set-background-for-terminal)
+
+(require 'base16-material-darker-theme)
 
 ;;; Backup and version control
 (setq make-backup-files nil)
+
+(use-package magit
+  :ensure t)
